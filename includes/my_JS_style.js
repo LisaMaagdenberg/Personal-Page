aea8a4243d59f8d8733eed8b7b8b68c91638d42e
Lisa Maagdenberg <lisa.maagdenberg92@gmail.com>
2017-01-15 13:08:56 +0100

    Working singe page navigation

---
+++ includes/my_JS_style.js
@@ -0,0 +1,24 @@
+function About() {
+	$('.contentPortfolio').hide();
+	$('.contentContact').hide();
+}
+
+$(document).ready(About);
+
+$('.About').on('click', function() {
+	$('.contentAbout').show();
+	$('.contentPortfolio').hide();
+	$('.contentContact').hide();
+})
+
+$('.Portfolio').on('click', function() {
+	$('.contentAbout').hide();
+	$('.contentPortfolio').show();
+	$('.contentContact').hide();
+})
+
+$('.Contact').on('click', function() {
+	$('.contentAbout').hide();
+	$('.contentPortfolio').hide();
+	$('.contentContact').show();
+})
